060334776864079469e589348885c4e595bde872
Rakesh kumar <Rakeshkr479@gmail.com>
2023-03-22 00:40:01 +0530

    2nd

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
 //import "../styles/App.css";
-import MyImage from '../images/1622569441093.jpg';
+import MyImage from '../images/1.png';
 function Home() {
   return (
     <>
     <div className='container'>
       <div className='row'>
         <div className='card'>
-        <div className='col-md-6 mt-4'>
-          <h1>Hello Guys <br></br>I am Rakesh kumar</h1>
+        <div className='col-md-6 mt'>
+          <h1 className='fw700 fs50'>Say hi to time tracking made easy with automation</h1>
+          <p className='p1'>A powerful tool for managing your team's time and productivity, with advanced workforce management capabilities to boost performance and results</p>
+          <button className='btn btn-danger'>Know More</button>
         </div>
         <div className='col-md-6'>
           <img src={MyImage} alt="horse" />
