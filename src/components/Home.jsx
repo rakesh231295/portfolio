9d9e0b9bbc3421548b3f02ae508a37b304021976
Rakesh kumar <Rakeshkr479@gmail.com>
2023-03-23 01:02:04 +0530

    er

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -18,6 +18,22 @@ function Home() {
         </div>
       </div>
     </div>
+    
+    <div className='container'>
+      <div className='row'>
+        <div className='card'>
+        <div className='col-md-6'>
+          <img src={MyImage} alt="horse" />
+        </div>
+        <div className='col-md-6 mt'>
+          <h1 className='fw700 fs50'>Say hi to time tracking made easy with automation</h1>
+          <p className='p1'>A powerful tool for managing your team's time and productivity, with advanced workforce management capabilities to boost performance and results</p>
+          <button className='btn btn-danger'>Know More</button>
+        </div>
+        
+        </div>
+      </div>
+    </div>
     </>
   )
 }
