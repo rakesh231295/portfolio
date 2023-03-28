9a9589b0ef05871ea6ccc280b80e2ca2688e1010
Rakesh kumar <Rakeshkr479@gmail.com>
2023-03-28 22:36:47 +0530

    56

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 function About() {
   return (
+    <>
     <div>About</div>
+    <div>About</div>
+    <div>About</div>
+    <div>About</div>
+    <div>About</div>
+    </>
   )
 }
 
